eb5ee0d5794b7c2b2780b81502dd3131ddc0b53e
Harsh Anand <167998439+harshanandsingh@users.noreply.github.com>
2025-04-17 11:11:08 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Find Pair Given Difference/find-pair-given-difference.cpp
@@ -0,0 +1,71 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+class Array {
+  public:
+    template <class T>
+    static void input(vector<T> &A, int n) {
+        for (int i = 0; i < n; i++) {
+            scanf("%d ", &A[i]);
+        }
+    }
+
+    template <class T>
+    static void print(vector<T> &A) {
+        for (int i = 0; i < A.size(); i++) {
+            cout << A[i] << " ";
+        }
+        cout << endl;
+    }
+};
+
+
+// } Driver Code Ends
+
+
+class Solution {
+  public:
+    bool findPair(vector<int> &arr, int x) {
+        // code here
+        unordered_set<int>s;
+        
+        int n = arr.size();
+        if(n==1) return false;
+        for(int i=0;i<n;i++){
+            if((s.find(arr[i]+x)!= s.end()) || (s.find(arr[i]-x) !=s.end())) return true;
+            s.insert(arr[i]);
+        }
+        return false;
+    }
+};
+
+
+
+//{ Driver Code Starts.
+
+int main() {
+
+    int t;
+    scanf("%d ", &t);
+
+    while (t--) {
+
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        int k;
+        scanf("%d ", &k);
+        Solution obj;
+
+        cout << (obj.findPair(arr, k) ? "true" : "false") << endl;
+        cout << "~" << endl;
+    }
+}
+
+// } Driver Code Ends

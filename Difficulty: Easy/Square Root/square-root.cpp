3e6afdc62d0b9d6829de715d1420425ce8af6edc
Harsh Anand <167998439+harshanandsingh@users.noreply.github.com>
2025-05-01 17:04:49 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Square Root/square-root.cpp
+++ Difficulty: Easy/Square Root/square-root.cpp
@@ -11,7 +11,16 @@ class Solution {
   public:
     int floorSqrt(int n) {
         // Your code goes here
-        int ans = sqrt(n);
+        int  i=1,j=n/2+1;
+        int ans = 0;
+        while(i<=j){
+            int mid = i + (j-i)/2;
+            if(mid *mid <=n){
+                ans=max(mid,ans);
+                i=mid+1;
+            }
+            else j=mid-1;
+        }
         return ans;
     }
 };

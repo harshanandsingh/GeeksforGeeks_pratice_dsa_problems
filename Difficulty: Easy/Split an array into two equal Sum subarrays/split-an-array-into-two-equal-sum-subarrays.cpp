c271f59f2443dce41d0ad97e54bd39ae9ae637e4
Harsh Anand <167998439+harshanandsingh@users.noreply.github.com>
2025-05-04 09:15:57 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Split an array into two equal Sum subarrays/split-an-array-into-two-equal-sum-subarrays.cpp
+++ Difficulty: Easy/Split an array into two equal Sum subarrays/split-an-array-into-two-equal-sum-subarrays.cpp
@@ -8,17 +8,14 @@ using namespace std;
 class Solution {
   public:
     bool canSplit(vector<int>& arr) {
-        int sum=0;
+        // code here
         int n = arr.size();
+        int sum = accumulate(arr.begin(),arr.end(),0);
         
-        for(int x:arr){
-            sum+=x;
-        }
-        
-        int cur_sum=0;
-        for(int i=0;i<n-1;i++){
-            cur_sum+=arr[i];
-            if(sum - cur_sum == cur_sum) return true;
+        int pre_sum =0;
+        for(int i=0;i<n;i++){
+            pre_sum +=arr[i];
+            if(pre_sum == sum - pre_sum) return true;
         }
         return false;
     }

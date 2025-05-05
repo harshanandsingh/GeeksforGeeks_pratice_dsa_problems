85aa25422794bd78b6db3075c993c34ef24bae40
Harsh Anand <167998439+harshanandsingh@users.noreply.github.com>
2025-05-05 12:19:41 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Flattening a Linked List/flattening-a-linked-list.cpp
+++ Difficulty: Medium/Flattening a Linked List/flattening-a-linked-list.cpp
@@ -1,36 +1,29 @@
 //{ Driver Code Starts
+//Initial Template for C++
+
 #include <bits/stdc++.h>
 using namespace std;
 
 struct Node {
     int data;
-    struct Node* next;
-    struct Node* bottom;
+    struct Node * next;
+    struct Node * bottom;
 
     Node(int x) {
         data = x;
         next = NULL;
         bottom = NULL;
     }
+
 };
 
-void printList(Node* head) {
-    while (head != NULL) {
-        printf("%d ", head->data);
-        head = head->bottom;
+void printList(Node *Node)
+{
+    while (Node != NULL)
+    {
+        printf("%d ", Node->data);
+        Node = Node->bottom;
     }
-    printf("\n");
-}
-
-Node* createLinkedList(vector<Node*>& v) {
-    Node* head = new Node(0);
-    Node* temp = head;
-    int n = v.size();
-    for (int i = 0; i < n; i++) {
-        temp->next = v[i];
-        temp = temp->next;
-    }
-    return head->next;
 }
 
 
@@ -54,8 +47,7 @@ struct Node{
 
 class Solution {
   public:
-    // Function which returns the  root of the flattened linked list.
-    Node* merge(Node* a, Node* b) {
+  Node* merge(Node* a, Node* b) {
         if (!a) return b;
         if (!b) return a;
     
@@ -82,6 +74,7 @@ class Solution {
         root = merge(root, root->next);
     
         return root;
+        
     }
 };
 
@@ -89,40 +82,69 @@ class Solution {
 
 //{ Driver Code Starts.
 
-int main() {
+int main(void) {
+
     int t;
     cin >> t;
-    cin.ignore();
     while (t--) {
-        int n;
+        int n, m, flag = 1, flag1 = 1;
+        struct Node * temp = NULL;
+        struct Node * head = NULL;
+        struct Node * pre = NULL;
+        struct Node * tempB = NULL;
+        struct Node * preB = NULL;
         cin >> n;
-        cin.ignore();
-
-        vector<Node*> v(n);
-
+        int work[n];
+        for (int i = 0; i < n; i++)
+            cin >> work[i];
         for (int i = 0; i < n; i++) {
-            string line;
-            getline(cin, line);
-            stringstream ss(line);
+            m = work[i];
+            --m;
+            int data;
+            scanf("%d", &data);
+            temp = new Node(data);
+            temp->next = NULL;
+            temp->bottom = NULL;
 
-            Node* head = new Node(0);
-            Node* temp = head;
-            int x;
-            while (ss >> x) {
-                Node* newNode = new Node(x);
-                temp->bottom = newNode;
-                temp = temp->bottom;
+            if (flag) {
+                head = temp;
+                pre = temp;
+                flag = 0;
+                flag1 = 1;
             }
-            v[i] = head->bottom;
+            else {
+                pre->next = temp;
+                pre = temp;
+                flag1 = 1;
+            }
+            for (int j = 0; j < m; j++) {
+
+                int temp_data;
+                scanf("%d", &temp_data);
+                tempB = new Node(temp_data);
+
+                if (flag1) {
+                    temp->bottom = tempB;
+                    preB = tempB;
+                    flag1 = 0;
+                }
+                else {
+                    preB->bottom = tempB;
+                    preB = tempB;
+                }
+            }
         }
+        Node *fun = head;
+        Node *fun2 = head;
 
         Solution ob;
-        Node* list = createLinkedList(v);
-        Node* head = ob.flatten(list);
-        printList(head);
-        cout << "~" << endl;
-    }
+        Node* root = ob.flatten(head);
+        printList(root);
+        cout << endl;
 
+    
+cout << "~" << "\n";
+}
     return 0;
 }
 

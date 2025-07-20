5948d5c4e6e7cb82af5b8b939edbfd3e38848168
Harsh Anand <167998439+harshanandsingh@users.noreply.github.com>
2025-07-20 11:30:36 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Max Sum Subarray of size K/max-sum-subarray-of-size-k.cpp
+++ Difficulty: Easy/Max Sum Subarray of size K/max-sum-subarray-of-size-k.cpp
@@ -1,51 +1,23 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-
 class Solution {
   public:
     int maximumSumSubarray(vector<int>& arr, int k) {
         // code here
-        int i=0,j=0;
         int n = arr.size();
-        int sum=0,max_=0;
+        
+        int max_sum = INT_MIN;
+        int sum=0;
+        int j=0,i=0;
         while(j<n){
             sum+=arr[j];
-            if(j-i+1 > k ) sum-=arr[i++];
-            max_=max(max_,sum);
+            
+            // if(j-i+1 < k) j++;
+            if(j-i+1 > k) sum-=arr[i++];
+            
+            max_sum = max(sum,max_sum);
+            
+            
             j++;
         }
-        return max_;
+        return max_sum;
     }
 };
-
-
-//{ Driver Code Starts.
-int main() {
-    string ts;
-    getline(cin, ts);
-    int t = stoi(ts);
-    while (t--) {
-
-        vector<int> arr;
-        string input;
-        getline(cin, input);
-        stringstream ss(input);
-        int number;
-        while (ss >> number) {
-            arr.push_back(number);
-        }
-        string ks;
-        getline(cin, ks);
-        int k = stoi(ks);
-        Solution ob;
-        int ans = ob.maximumSumSubarray(arr, k);
-        cout << ans << "\n";
-        cout << "~" << endl;
-    }
-    return 0;
-}
-// } Driver Code Ends

1d186d71f92bcaa1075d13bc194c279faf17af53
Harsh Anand <167998439+harshanandsingh@users.noreply.github.com>
2025-09-24 18:14:35 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Rotate Array by One/rotate-array-by-one.cpp
+++ Difficulty: Basic/Rotate Array by One/rotate-array-by-one.cpp
@@ -1,12 +1,3 @@
-//{ Driver Code Starts
-// Initial Template for C++
-
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-
 // User function Template for C++
 
 class Solution {
@@ -14,37 +5,10 @@ class Solution {
     void rotate(vector<int> &arr) {
         // code here
         int n = arr.size();
-        int k = arr[n-1];
-        for(int i=n;i>0;i--) arr[i]=arr[i-1];
-        arr[0]=k;
+        int last = arr[n-1];
+        for(int i=n-1;i>0;i--){
+            arr[i]= arr[i-1];
+        }
+        arr[0]=last;
     }
 };
-
-
-//{ Driver Code Starts.
-
-int main() {
-    int t;
-    cin >> t;
-    cin.ignore();
-    while (t--) {
-        vector<int> arr;
-        string input;
-        getline(cin, input);
-        stringstream ss(input);
-        int number;
-        while (ss >> number) {
-            arr.push_back(number);
-        }
-        Solution ob;
-        ob.rotate(arr);
-        for (long long i = 0; i < arr.size(); i++) {
-            cout << arr[i] << " ";
-        }
-        cout << endl;
-        cout << "~" << endl;
-    }
-    return 0;
-}
-
-// } Driver Code Ends

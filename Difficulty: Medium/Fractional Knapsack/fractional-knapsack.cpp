e711d0a2eb3042c6fe940090020967ccd072d438
Harsh Anand <167998439+harshanandsingh@users.noreply.github.com>
2025-09-30 09:04:29 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Fractional Knapsack/fractional-knapsack.cpp
+++ Difficulty: Medium/Fractional Knapsack/fractional-knapsack.cpp
@@ -1,97 +1,33 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-
-// class implemented
-/*
-struct Item{
-    int value;
-    int weight;
-};
-*/
-
 class Solution {
   public:
     double fractionalKnapsack(vector<int>& val, vector<int>& wt, int capacity) {
         // code here
-        
-        int n = val.size(); // fixed 'sizd' typo
-        vector<pair<double, pair<int, int>>> res; // {ratio, {weight, value}}
-        
-        for (int i = 0; i < n; i++) {
-            double ratio = (double)val[i] / wt[i];
-            res.push_back({ratio, {wt[i], val[i]}});
+        vector<pair<int,int>>cap;
+        for(int i=0;i<val.size();i++){
+            cap.push_back({val[i],wt[i]});
         }
         
-        // Sort in descending order of ratio
-        sort(res.rbegin(), res.rend());
+        sort(cap.begin(),cap.end(),[](pair<int,int>&a , pair<int,int>&b){
+            return (double) a.first/a.second > (double)b.first/b.second;
+        });
         
-        double sum = 0.0;
         
-        for (int i = 0; i < n; i++) {
-            int currWt = res[i].second.first;
-            int currVal = res[i].second.second;
-            
-            if (capacity >= currWt) {
-                // Take full item
-                sum += currVal;
-                capacity -= currWt;
-            } else {
-                // Take fraction of the item
-                sum += res[i].first * capacity; // ratio * remaining capacity
-                break; // knapsack is now full
+        int i=0;
+        double profit = 0;
+        while(capacity>0 && i<cap.size()){
+            if(cap[i].second <= capacity){
+                profit +=cap[i].first;
+                capacity -= cap[i].second;
             }
+            else{
+                profit += ((double)cap[i].first/cap[i].second) * capacity;
+                capacity = 0;
+            }
+            i++;
         }
+        return profit;
         
-        return (sum * 1e6) / 1e6;
+        
+        
     }
 };
-
-
-
-//{ Driver Code Starts.
-
-int main() {
-
-    int t;
-    // taking testcases
-    cin >> t;
-    cin.ignore(); // to ignore the newline after the number of test cases
-    cout << setprecision(6) << fixed;
-
-    while (t--) {
-        // Reading the value array
-        vector<int> values;
-        string input;
-        getline(cin, input);
-        stringstream ss(input);
-        int number;
-        while (ss >> number) {
-            values.push_back(number);
-        }
-
-        // Reading the weight array
-        vector<int> weights;
-        getline(cin, input);
-        stringstream ss2(input);
-        while (ss2 >> number) {
-            weights.push_back(number);
-        }
-
-        // Reading the capacity
-        int w;
-        cin >> w;
-        cin.ignore(); // to ignore the newline after capacity
-
-        // function call
-        Solution ob;
-        cout << ob.fractionalKnapsack(values, weights, w) << endl;
-        cout << "~" << endl;
-    }
-    return 0;
-}
-
-// } Driver Code Ends

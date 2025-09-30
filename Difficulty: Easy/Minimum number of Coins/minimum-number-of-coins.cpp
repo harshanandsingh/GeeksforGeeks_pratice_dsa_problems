89009066a9c12a29d11f1ff5d786a6361bb12062
Harsh Anand <167998439+harshanandsingh@users.noreply.github.com>
2025-09-30 09:27:44 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Minimum number of Coins/minimum-number-of-coins.cpp
+++ Difficulty: Easy/Minimum number of Coins/minimum-number-of-coins.cpp
@@ -1,51 +1,16 @@
-//{ Driver Code Starts
-// Initial Template for C++
-
-#include <bits/stdc++.h>
-using namespace std;
-
-// } Driver Code Ends
-
-// User function Template for C++
-
 class Solution {
   public:
-    vector<int> minPartition(int N) {
+    int findMin(int n) {
         // code here
-        vector<int>v{1,2,5,10,20,50,100,200,500,2000};
+        vector<int>coin = {10,5,2,1};
         
-        vector<int>ans;
-        
-        int n = v.size();
-        for(int i=n-1;i>=0;i--){
-            while(N>=v[i]){
-                ans.push_back(v[i]);
-                N-=v[i];
-            }
-            if(N==0) break;
+        int total_coin = 0;
+        int i=0;
+        while(n>0){
+            total_coin += n/coin[i];
+            n %= coin[i];
+            i++;
         }
-        return ans;
+        return total_coin;
     }
 };
-
-
-//{ Driver Code Starts.
-
-int main(){
-    int t;
-    cin>>t;
-    while(t--){
-        int N;
-        cin>>N;
-        
-        Solution ob;
-        vector<int> numbers = ob.minPartition(N);
-        for(auto u: numbers)
-            cout<<u<<" ";
-        cout<<"\n";
-    
-cout << "~" << "\n";
-}
-    return 0;
-}
-// } Driver Code Ends
